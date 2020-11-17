6adf5e4f5755fea232cccf4f21288407b258ff6f
Cedric <leeced@umich.edu>
2020-11-17 12:34:16 -0500

    created psql init file

---
+++ server/db_init.sql
@@ -0,0 +1,31 @@
+CREATE TABLE Applications (
+  _id SERIAL PRIMARY KEY,
+  company_id INT NOT NULL,
+  role_id INT NOT NULL,
+  location_id INT NOT NULL,
+  applied_date DATE NOT NULL,
+  deadline DATE NOT NULL,
+  cover_letter_submitted BOOLEAN NOT NULL,
+  job_url VARCHAR NOT NULL,
+  submitted BOOLEAN NOT NULL,
+  received_phone_screen BOOLEAN NOT NULL,
+  received_on_site BOOLEAN NOT NULL,
+  FOREIGN KEY (company_id) REFERENCES companies (_id),
+  FOREIGN KEY (role_id) REFERENCES roles (_id),
+  FOREIGN KEY (location_id) REFERENCES locations (_id)
+);
+
+CREATE TABLE Companies (
+  _id VARCHAR PRIMARY KEY, 
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE Roles (
+  _id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL
+);
+
+CREATE TABLE Locations (
+  _id SERIAL PRIMARY KEY,
+  name VARCHAR NOT NULL
+);
