c0fd21a2af67006bd2dc900ab08affca08ce3741
shreshth <shreshthsrivastava2@gmail.com>
2020-11-18 12:25:35 -0500

    working on redoing styling for new application page. kaiwei is close to having the check boxes ready. gradient has been added to background and input fields have been restyled. need to add back box shadow for navbar

--- client/newapplication/NewApplication.jsx
+++ client/newapplication/NewApplication.jsx
@@ -4,107 +4,92 @@ import './newapplication.scss';
 
 const NewApplication = () => {
   return (
-    <div>
-      <form id="application-form" action="/new-application-form" method="POST">
-        <label htmlFor="companyName">Company Name</label>
-        <input
-          type="text"
-          id="companyName"
-          name="companyName"
-          placeholder="Company name..."
-          required
-        />
-        <br />
-        <label htmlFor="role">Role for this job application</label>
-        <input
-          type="text"
-          id="role"
-          name="role"
-          placeholder="Name of the role..."
-          required
-        />
-        <br />
-        <label htmlFor="date">Application Date</label>
-        <input
-          type="text"
-          id="date"
-          name="date"
-          placeholder="Application date..."
-          required
-        />
-        <br />
-        <label htmlFor="deadline">Application Deadline</label>
-        <input
-          type="text"
-          id="deadline"
-          name="deadline"
-          placeholder="Application deadline..."
-          required
-        />
-        <br />
-        <label htmlFor="location">Job location</label>
-        <input
-          type="text"
-          id="location"
-          name="location"
-          placeholder="Job location..."
-          required
-        />
-        <br />
-        <label htmlFor="url">Job application URL</label>
-        <input
-          type="text"
-          id="url"
-          name="url"
-          placeholder="Application URL..."
-        />
-        <br />
-        <label htmlFor="coverLetter">
-          Have you submitted your cover letter for this job
-        </label>
-        <input
-          type="checkbox"
-          id="applied"
-          name="coverLetter"
-          value="true"
-          className="checkbox"
-        />
-        <br />
-        <label htmlFor="applied">
-          Have you submitted your application for this job
-        </label>
-        <input
-          type="checkbox"
-          id="applied"
-          name="applied"
-          value="true"
-          className="checkbox"
-        />
-        <br />
-        <label htmlFor="phoneScreen">
-          Have you received phone screen for this job application?
-        </label>
-        <input
-          type="checkbox"
-          id="phoneScreen"
-          name="phoneScreen"
-          value="true"
-          className="checkbox"
-        />
-        <br />
-        <label htmlFor="onsite">
-          Have you received onsite interview for this job application?
-        </label>
-        <input
-          type="checkbox"
-          id="onsite"
-          name="onsite"
-          value="true"
-          className="checkbox"
-        />
-        <br />
-        <input type="submit" id="submit" value="Submit" />
-      </form>
+    <div className="new-application-background">
+      <div className="form-div">
+        <form
+          id="application-form"
+          action="/new-application-form"
+          method="POST"
+        >
+          <h3 className="heading">Add a new application</h3>
+          <label htmlFor="companyName">Company Name</label>
+          <br />
+          <input type="text" id="companyName" name="companyName" required />
+          <br />
+          <label htmlFor="role">Role for this job application</label>
+          <br />
+          <input type="text" id="role" name="role" required />
+          <br />
+          <label htmlFor="date">Application Date</label>
+          <br />
+          <input type="text" id="date" name="date" required />
+          <br />
+          <label htmlFor="deadline">Application Deadline</label>
+          <br />
+          <input type="text" id="deadline" name="deadline" required />
+          <br />
+          <label htmlFor="location">Job location</label>
+          <br />
+          <input type="text" id="location" name="location" required />
+          <br />
+          <label htmlFor="url">Job application URL</label>
+          <br />
+          <input type="text" id="url" name="url" />
+          <br />
+          <label htmlFor="coverLetter" className="container">
+            Have you submitted your cover letter for this job
+            <input
+              type="checkbox"
+              id="applied"
+              name="coverLetter"
+              o
+              value="true"
+              className="checkbox"
+            />
+            <span className="checkmark"></span>
+          </label>
+          <br />
+          <label htmlFor="applied" className="container">
+            Have you submitted your application for this job
+            <input
+              type="checkbox"
+              id="applied"
+              name="applied"
+              value="true"
+              className="checkbox"
+            />
+            <span className="checkmark"></span>
+          </label>
+          <br />
+          <label htmlFor="phoneScreen" className="container">
+            Have you received phone screen for this job application?
+            <input
+              type="checkbox"
+              id="phoneScreen"
+              name="phoneScreen"
+              value="true"
+              className="checkbox"
+            />
+            <span className="checkmark"></span>
+          </label>
+          <br />
+          <label htmlFor="onsite" className="container">
+            Have you received onsite interview for this job application?
+            <input
+              type="checkbox"
+              id="onsite"
+              name="onsite"
+              value="true"
+              className="checkbox"
+            />
+            <span className="checkmark"></span>
+          </label>
+          <br />
+          <div className="submit-div">
+            <input type="submit" id="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
